f62112dc36e128a7b131e3d504c8a77b2be5e57b
Jinkyu Sung <jinkyusung@icloud.com>
2024-09-22 21:38:18 +0900

    [UPDATE] `util.py`
    `pickle` file loader and saver.

--- cf_frame/util.py
+++ cf_frame/util.py
@@ -4,6 +4,7 @@ import random
 import logging
 import importlib
 import numpy as np
+import pickle
 from cf_frame.configurator import args
 
 def init_seed():
@@ -115,3 +116,16 @@ class DisabledSummaryWriter:
         return self
     def __getattr__(self, *args, **kwargs):
         return self
+    
+
+def pload(path):
+	with open(path, 'rb') as f:
+		res = pickle.load(f)
+	print('load path = {} object'.format(path))
+	return res
+
+
+def pstore(x, path):
+	with open(path, 'wb') as f:
+		pickle.dump(x, f)
+	print('store object in path = {} ok'.format(path))
