4141be1b84cc50fc850f934950afc39ff959616a
chanwoo <chanwoo.kim@snu.ac.kr>
2024-10-31 19:07:28 +0900

    [FIX] summary available for nonparam trainer

--- cf_frame/trainer/nonparam.py
+++ cf_frame/trainer/nonparam.py
@@ -1,3 +1,4 @@
+from cf_frame.configurator import args
 from cf_frame.util import log_exceptions
 from cf_frame.trainer import BaseTrainer
 import time
@@ -14,7 +15,14 @@ class NonParam(BaseTrainer):
         self.evaluate(model)
         return model
     
-    # if test needed, delete this method
+    # if test needed, delete below two methods
+    @log_exceptions
+    def evaluate(self, model, epoch_idx=None):
+        eval_result = super().evaluate(model, epoch_idx)
+        if args.summary is not None:
+            self.logger.log_summary(eval_result, args.ks)
+        return eval_result
+    
     @log_exceptions
     def test(self, model):
         pass
